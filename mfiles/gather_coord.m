8c380e3bf6f3f3ed7d33c15dcfa6cdd99dea1550
ZHANG Wei <zhangwei.zw@gmail.com>
2021-08-14 07:09:06 +0800

    Read either 3d curv grid or 1d cart grid.

--- mfiles/gather_coord.m
+++ mfiles/gather_coord.m
@@ -5,6 +5,9 @@ function [x,y,z] = gather_coord(coordinfo,varargin)
 % Email:    yhhuo@mail.ustc.edu.cn
 % Date:     2021.05.31
 
+% ChangeLog:
+% 20210814: read both cart and curv coord, --zw
+
 %-- flags --
 nargs=nargin-1;
 n=1;
@@ -44,9 +47,45 @@ for n=1:nthd
     end
 
     subs=fliplr(subs);subc=fliplr(subc);subt=fliplr(subt);
-    x(k1:k2,j1:j2,i1:i2)=nc_varget(fnm_coord,'x',subs-1,subc,subt);
-    y(k1:k2,j1:j2,i1:i2)=nc_varget(fnm_coord,'y',subs-1,subc,subt);
-    z(k1:k2,j1:j2,i1:i2)=nc_varget(fnm_coord,'z',subs-1,subc,subt);
+
+    % check dimension size of x,y,z to detmine cart or curv
+    xvar_info = ncinfo(fnm_coord,'x');
+    num_dim_x = length(xvar_info.Dimensions);
+    if num_dim_x == 1 % cart grid
+      x1d = nc_varget(fnm_coord,'x',subs(3)-1,subc(3),subt(3));
+      x3d = repmat(x1d,1,(j2-j1+1),(k2-k1+1));
+      x(k1:k2,j1:j2,i1:i2) = permute(x3d,[3 2 1]);
+    else % curv grid
+      x(k1:k2,j1:j2,i1:i2)=nc_varget(fnm_coord,'x',subs-1,subc,subt);
+    end
+
+    %- y coord
+    yvar_info = ncinfo(fnm_coord,'y');
+    num_dim_y = length(yvar_info.Dimensions);
+    if num_dim_y == 1 % cart grid
+      y1d = nc_varget(fnm_coord,'y',subs(2)-1,subc(2),subt(2));
+      y3d = repmat(y1d,1,(i2-i1+1),(k2-k1+1)); % y1d is a col, can't be rep as 1st dim
+      y(k1:k2,j1:j2,i1:i2) = permute(y3d,[3 1 2]);
+    else % curv grid
+      y(k1:k2,j1:j2,i1:i2)=nc_varget(fnm_coord,'y',subs-1,subc,subt);
+    end
+
+    %- z coord
+    zvar_info = ncinfo(fnm_coord,'z');
+    num_dim_z = length(zvar_info.Dimensions);
+    if num_dim_z == 1 % cart grid
+      z1d = nc_varget(fnm_coord,'z',subs(1)-1,subc(1),subt(1));
+      z3d = repmat(z1d,1,(j2-j1+1),(i2-i1+1));
+      %z(k1:k2,j1:j2,i1:i2) = permute(z3d,[1 2 3]);
+      z(k1:k2,j1:j2,i1:i2) = z3d;
+    else % curv grid
+      z(k1:k2,j1:j2,i1:i2)=nc_varget(fnm_coord,'z',subs-1,subc,subt);
+    end
+
+    % only for curv
+    %x(k1:k2,j1:j2,i1:i2)=nc_varget(fnm_coord,'x',subs-1,subc,subt);
+    %y(k1:k2,j1:j2,i1:i2)=nc_varget(fnm_coord,'y',subs-1,subc,subt);
+    %z(k1:k2,j1:j2,i1:i2)=nc_varget(fnm_coord,'z',subs-1,subc,subt);
     
 end
 
