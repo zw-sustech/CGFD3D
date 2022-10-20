5ec6635805463f396a23ad8c037dd3f514d6fb31
Hongtao Gao <hongtaogao@pku.edu.cn>
2022-10-20 07:24:41 +0000

    update example/prep_source/stf_rickerderiv.m. modify the derivative of the ricker wavelet function
    Signed-off-by: Hongtao Gao <hongtaogao@pku.edu.cn>

--- example/prep_source/stf_rickerderiv.m
+++ example/prep_source/stf_rickerderiv.m
@@ -1,6 +1,11 @@
 function S=stf_rickerderiv(t,fc,t0)
-
-f0=sqrt(pi)/2.0;
+%--------------------------------------------
+%-- Discription:  To generate ricker wavelet
+%-- Input:        t: time
+%                 fc: dominant frequency
+%                 t0: initial time
+%-- Output:       S
+%--------------------------------------------
 u=(t-t0)*2.0*pi*fc;
-S=u.*(1.5-u.^2/4.0).*exp(-u.^2.0/4.0)*f0*pi*fc;
+S=u.*(-3*pi*fc+1/2*pi*fc*u.^2).*exp(-u.^2.0/4.0);
 end
