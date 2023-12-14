ba6f0a9c7f6a60a17cf17207fe3c8c660c6c2700
jianglq <jlq0608@gmail.com>
2023-12-14 12:33:29 +0800

    geometry funcs for plane

--- media/media_geometry3d.cpp
+++ media/media_geometry3d.cpp
@@ -1,3 +1,28 @@
+/******************************************************************************
+ *
+ * 3D geometry functions.
+ *
+ * Authors: Luqian Jiang <jianglq@mail.ustc.edu.cn>
+ *          Wei Zhang <zhangwei@sustech.edu.cn>
+ *
+ * Copyright (c) 2021 zwlab
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License along 
+ * with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *******************************************************************************/
+
+
 #include <iostream>
 #include <vector>
 #include <cfloat>
@@ -45,22 +70,17 @@ float Angle(const Vector3 &A, const Vector3 &B) {
 	return acos(Dot(A, B)/Length(A)/Length(B));
 }
 
+// directional cosine of two vectors
+float CosinDir(const Vector3 &A, const Vector3 &B) {
+  return Dot(A, B)/Length(A)/Length(B);
+}
+
 float DistanceToPlane(const Point3 &p, const Point3 &p0, const Vector3 &n) {
 	return fabs(Dot(p-p0, n));
 }
 
-
 Point3 GetPlaneProjection(const Point3 &p, const Point3 &p0, const Vector3 &n) {
 	return p - n*Dot(p-p0, n);
-}
-
-Point3 LinePlaneIntersection(
-	const Point3 &p1, const Point3 &p2, const Point3 &p0, 
-    const Vector3 &n)
-{
-	Vector3 v = p2 - p1;
-	float t = (Dot(n, p0-p1) / Dot(n, p2-p1)); // denominator = 0?
-	return p1 + v*t;
 }
 
 Vector3 Cross(const Vector3 &A, const Vector3 &B) {
@@ -71,14 +91,70 @@ float Area2(const Point3 &A, const Point3 &B, const Point3 &C) {
 	return Length(Cross(B-A, C-A));
 }
 
+//==== for plane
+Plane::Plane(const Plane &P) {
+  a = P.a;
+  b = P.b;
+  c = P.c;
+  d = P.d;
+}
 
-bool isPointInTri(const Point3 &P, const Point3 &P0, const Point3 &P1, const Point3 &P2) {
+Plane::Plane(float a_, float b_, float c_, float d_) {
+  a = a_;
+  b = b_;
+  c = c_;
+  d = d_;
+}
+
+Plane::Plane(const Vector3 &NormalizedNormal, float d_)
+{
+  a = NormalizedNormal.x;
+  b = NormalizedNormal.y;
+  c = NormalizedNormal.z;
+  d = d_;
+}
+
+Plane Plane::Normalize()
+{
+  Plane Result;
+  float Distance = sqrtf(a * a + b * b + c * c);
+  Result.a = a / Distance;
+  Result.b = b / Distance;
+  Result.c = c / Distance;
+  Result.d = d / Distance;
+  return Result;
+}
+
+Point3 Plane::IntersectSegment(const Point3 &V1, const Point3 &V2, bool &isInter) const
+{
+  isInter = true;
+  Vector3 Diff = V1 - V2;
+  float Denominator = a * Diff.x + b * Diff.y + c * Diff.z;
+  if(Denominator == 0.0f) {
+    isInter = false;
+    return V1;
+  }
+  float u = (a * V1.x + b * V1.y + c * V1.z + d) / Denominator;
+
+  if (u > 1 || u < 0) isInter = false;
+
+  return (V1 + (V2 - V1)*u);
+}
+
+float Plane::DotNormal(const Plane &P, const Vector3 &V)
+{
+  return P.a * V.x + P.b * V.y + P.c * V.z;
+}
+
+
+/*
+bool PointInTri(Point3 P, Point3 P0, Point3 P1, Point3 P2) {
 	float area1 = Area2(P, P0, P1);
 	float area2 = Area2(P, P1, P2);
 	float area3 = Area2(P, P2, P0);
 	return dcmp(area1+area2+area3 - Area2(P0, P1, P2)) == 0;
 }
-/*
+
 float Volume6(Point3 A, Point3 B, Point3 C, Point3 D) {
 	return Dot(D-A, Cross(B-A, C-A));
 }
@@ -90,10 +166,6 @@ float Volume6(Point3 A, Point3 B, Point3 C, Point3 D) {
  */
 bool isPointInPolyhedron(const Point3 &p, const std::vector<Face> &fs) {
 	for (Face const &f:fs) {
-		// if the point on the triangle, return true 
-		if ( isPointInTri(p, f.v[0], f.v[1], f.v[2]) ) {
-			return true;
-		}
 		Vector3 p2f = f.v[0]-p;
 		Vector3 A = f.normal();
 		float sign = Dot(p2f, f.normal());
@@ -106,7 +178,7 @@ bool isPointInPolyhedron(const Point3 &p, const std::vector<Face> &fs) {
 }
 
 /*
- * Input: vx, vy, vz are the EIGHT vertices of the hexahedron 
+ * Input: vx, vy, vz are the EIGHT vertexes of the hexahedron 
  *
  *    ↑ +z       4----6
  *    |         /|   /|
@@ -114,6 +186,7 @@ bool isPointInPolyhedron(const Point3 &p, const std::vector<Face> &fs) {
  *            5----7 /
  *            |/   |/
  *            1----3
+ *
  *
  */
 bool isPointInHexahedron(float px, float py, float pz,
@@ -142,10 +215,10 @@ bool isPointInHexahedron(float px, float py, float pz,
 //		Face{ {Point3{vx[7], vy[7], vz[7]}, Point3{vx[6], vy[6], vz[6]}, Point3{vx[4], vy[4], vz[4]}} }, // top
 
 //		Face{ {Point3{vx[0], vy[0], vz[0]}, Point3{vx[2], vy[2], vz[2]}, Point3{vx[3], vy[3], vz[3]}} }, 
-		Face{ {Point3{vx[3], vy[3], vz[3]}, Point3{vx[1], vy[1], vz[1]}, Point3{vx[0], vy[0], vz[0]}} } // bottom
+		Face{ {Point3{vx[3], vy[3], vz[3]}, Point3{vx[1], vy[1], vz[1]}, Point3{vx[0], vy[0], vz[0]}} }, // bottom
 	};
 
-  return isPointInPolyhedron(P, hexa);
+    return isPointInPolyhedron(P, hexa);
 }
 
 
@@ -167,71 +240,72 @@ bool isPointInHexahedron_strict(float px, float py, float pz,
                                float *vx, float *vy, float *vz) 
 {
 
-	Point3 P(px, py, pz); 
+  Point3 P(px, py, pz); 
 
-	Point3 v0(vx[0], vy[0], vz[0]);
-	Point3 v1(vx[1], vy[1], vz[1]);
-	Point3 v2(vx[2], vy[2], vz[2]);
-	Point3 v3(vx[3], vy[3], vz[3]);
-	Point3 v4(vx[4], vy[4], vz[4]);
-	Point3 v5(vx[5], vy[5], vz[5]);
-	Point3 v6(vx[6], vy[6], vz[6]);
-	Point3 v7(vx[7], vy[7], vz[7]);
+  Point3 v0(vx[0], vy[0], vz[0]);
+  Point3 v1(vx[1], vy[1], vz[1]);
+  Point3 v2(vx[2], vy[2], vz[2]);
+  Point3 v3(vx[3], vy[3], vz[3]);
+  Point3 v4(vx[4], vy[4], vz[4]);
+  Point3 v5(vx[5], vy[5], vz[5]);
+  Point3 v6(vx[6], vy[6], vz[6]);
+  Point3 v7(vx[7], vy[7], vz[7]);
 
-	/* 
-	 * One hexahedron cut into five tetrahedron.
-	 * Since the four points may not coplanar, cut twice, ten tetrahedron.
-	 */
-	std::vector< std::vector<Face> > tetrahs(10, std::vector<Face>(4));
+  /* 
+   * One hexahedron cut into five tetrahedron.
+   * Since the four points may not coplanar, cut twice, ten tetrahedron.
+   */
+  std::vector< std::vector<Face> > tetrahs(10, std::vector<Face>(4));
 
-	// 0, 1, 2, 4
-	tetrahs[0] = { 
-		Face{{v0, v4, v2}}, Face{{v2, v4, v1}}, Face{{v1, v4, v0}}, Face{{v0, v2, v1}}
-	};
-	// 2, 6, 4, 7 
-	tetrahs[1] = { 
-		Face{{v4, v6, v2}}, Face{{v2, v6, v7}}, Face{{v7, v6, v4}}, Face{{v4, v2, v7}}
-	};
-	// 3, 1, 7, 2
-	tetrahs[2] = {
-		Face{{v2, v7, v3}}, Face{{v3, v7, v1}}, Face{{v1, v2, v3}}, Face{{v1, v7, v2}}
-	};
-	// 1, 7, 4, 5
-	tetrahs[3] = {
-		Face{{v7, v4, v5}}, Face{{v1, v7, v5}}, Face{{v5, v4, v1}}, Face{{v1, v4, v7}}
-	};
-	// 1, 4, 2, 7
-	tetrahs[4] = {
-		Face{{v2, v4, v7}}, Face{{v7, v4, v1}}, Face{{v2, v7, v1}}, Face{{v4, v2, v1}}
-	};
+  // 0, 1, 2, 4
+  tetrahs[0] = { 
+    Face{{v0, v4, v2}}, Face{{v2, v4, v1}}, Face{{v1, v4, v0}}, Face{{v0, v2, v1}}
+  };
+  // 2, 6, 4, 7 
+  tetrahs[1] = { 
+    Face{{v4, v6, v2}}, Face{{v2, v6, v7}}, Face{{v7, v6, v4}}, Face{{v4, v2, v7}}
+  };
+  // 3, 1, 7, 2
+  tetrahs[2] = {
+    Face{{v2, v7, v3}}, Face{{v3, v7, v1}}, Face{{v1, v2, v3}}, Face{{v1, v7, v2}}
+  };
+  // 1, 7, 4, 5
+  tetrahs[3] = {
+    Face{{v7, v4, v5}}, Face{{v1, v7, v5}}, Face{{v5, v4, v1}}, Face{{v1, v4, v7}}
+  };
+  // 1, 4, 2, 7
+  tetrahs[4] = {
+    Face{{v2, v4, v7}}, Face{{v7, v4, v1}}, Face{{v2, v7, v1}}, Face{{v4, v2, v1}}
+  };
 
 
-	// 4, 5, 6, 0
-	tetrahs[5] = {
-		Face{{v4, v5, v6}}, Face{{v4, v6, v0}}, Face{{v4, v0, v5}}, Face{{v6, v5, v0}}
-	};
-	// 0, 2, 6, 3
-	tetrahs[6] = {
-		Face{{v6, v2, v0}}, Face{{v6, v3, v2}}, Face{{v0, v2, v3}}, Face{{v3, v6, v0}}
-	};
-	// 5, 6, 3, 7
-	tetrahs[7] = {
-		Face{{v6, v5, v7}}, Face{{v3, v6, v7}}, Face{{v7, v5, v3}}, Face{{v5, v6, v3}}
-	};
-	// 5, 0, 1, 3
-	tetrahs[8] = {
-		Face{{v5, v0, v1}}, Face{{v3, v5, v1}}, Face{{v0, v3, v1}}, Face{{v0, v5, v3}}
-	};
-	// 0, 5, 6, 3
-	tetrahs[9] = {
-		Face{{v5, v6, v0}}, Face{{v5, v0, v3}}, Face{{v0, v6, v3}}, Face{{v3, v6, v5}}
-	};
+  // 4, 5, 6, 0
+  tetrahs[5] = {
+    Face{{v4, v5, v6}}, Face{{v4, v6, v0}}, Face{{v4, v0, v5}}, Face{{v6, v5, v0}}
+  };
+  // 0, 2, 6, 3
+  tetrahs[6] = {
+    Face{{v6, v2, v0}}, Face{{v6, v3, v2}}, Face{{v0, v2, v3}}, Face{{v3, v6, v0}}
+  };
+  // 5, 6, 3, 7
+  tetrahs[7] = {
+    Face{{v6, v5, v7}}, Face{{v3, v6, v7}}, Face{{v7, v5, v3}}, Face{{v5, v6, v3}}
+  };
+  // 5, 0, 1, 3
+  tetrahs[8] = {
+    Face{{v5, v0, v1}}, Face{{v3, v5, v1}}, Face{{v0, v3, v1}}, Face{{v0, v5, v3}}
+  };
+  // 0, 5, 6, 3
+  tetrahs[9] = {
+    Face{{v5, v6, v0}}, Face{{v5, v0, v3}}, Face{{v0, v6, v3}}, Face{{v3, v6, v5}}
+  };
 
-	for (auto &tetrah:tetrahs) {
-		if (isPointInPolyhedron(P, tetrah)) {
-				return true;
-		}
-	}
+  for (auto &tetrah:tetrahs) {
+    if (isPointInPolyhedron(P, tetrah)) {
+        return true;
+    }
+  }
 
   return false;
 }
+
--- media/media_geometry3d.hpp
+++ media/media_geometry3d.hpp
@@ -1,3 +1,26 @@
+/***********************************************************************
+ *
+ * Authors: Luqian Jiang <jianglq@mail.ustc.edu.cn>
+ *          Wei Zhang <zhangwei@sustech.edu.cn>
+ *
+ * Copyright (c) 2021 zwlab
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License along 
+ * with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************/
+
+
 #ifndef _MEDIA_GEOMETRY3D_H_
 #define _MEDIA_GEOMETRY3D_H_
 
@@ -5,12 +28,23 @@
 #include <vector>
 #include <cfloat>
 #include <cassert>
-
-#define EPS 1e-10
+#include <unordered_map>
+#define EPS 1e-6
 
 struct Point3{
 	float x, y, z;
 	Point3(float x = 0, float y = 0, float z = 0):x(x), y(y), z(z) {}
+
+  // just used for set, no actual meaning
+  bool operator<(const Point3 &A) const {
+    return x < A.x || (fabs(x-A.x)<FLT_EPSILON && y<A.y) || 
+                      (fabs(x-A.x)<FLT_EPSILON && fabs(y-A.y)<FLT_EPSILON && z < A.z);
+  }
+  bool operator==(const Point3 &A) const {
+      return (fabs(this->x-A.x)<=FLT_EPSILON && 
+              fabs(this->y-A.y)<=FLT_EPSILON && 
+              fabs(this->z-A.z)<=FLT_EPSILON);
+  } 
 };
 
 typedef Point3 Vector3;
@@ -23,6 +57,7 @@ Vector3 operator / (const Vector3 &A, float p);
 float Dot(const Vector3 &A, const Vector3 &B);
 float Length(const Vector3 &A);
 float Angle(const Vector3 &A, const Vector3 &B);
+float CosinDir(const Vector3 &A, const Vector3 &B);
 float DistanceP2P(const Point3 &A, const Point3 &B);
 float DistanceToPlane(const Point3 &p, const Point3 &p0, const Vector3 &n);
 Point3 GetplaneProjection(const Point3 &p, const Point3 &p0, const Vector3 &n);
@@ -31,42 +66,63 @@ Point3 LinePlaneIntersection(
     const Vector3 &n);
 Vector3 Cross(const Vector3 &A, const Vector3 &B);
 float Area2 (const Point3 &A, const Point3 &B, const Point3 &C);
-bool isPointInTri(const Point3 &P, const Point3 &P0, const Point3 &P1, const Point3 &P2);
+//bool PointInTri(conPoint3 P, Point3 P0, Point3 P1, Point3 P2);
 //float Volume6(Point3 A, Point3 B, Point3 C, Point3 D);
+
+// A standard 3D plane, a*x + b*y + c*z + d = 0
+struct Plane {
+  float a, b, c, d;  
+  Plane() = default;
+  ~Plane() = default;
+  Plane(const Plane &P);
+  Plane(float a_, float b_, float c_, float d_);
+  Plane(const Vector3 &NormalizedNormal, float d_);
+  Plane Normalize();
+
+  //== intersection
+  //determines the intersect of the line defined by the points V1 and V2 with the plane.
+  //Returns the point of intersection.  Origin is returned if no intersection exists.
+  Point3 IntersectSegment(const Point3 &V1, const Point3 &V2, bool &isInter) const;
+
+  float DotNormal(const Plane &P, const Vector3 &V);    //dot product of a plane and a 3D normal
+};
+
 
 /* 
  * If the number of points is 3, calculate the normal directly;
  * if the number of points > 3, just use first two points.
  */
 struct Face {
-    std::vector<Point3> v;
-    Vector3 normal() const { 
-        assert(v.size() > 2);
-        Vector3 n = Cross(v[1]-v[0], v[2]-v[0]);
-        /* Normalized the normal vector */
-        float d = Length(n);
-        n = n/d;
-        return n;
-    }
-    Face() = default;
-     ~Face() = default;
+  std::vector<Point3> v;
+  Vector3 normal() const { 
+    assert(v.size() > 2);
+    Vector3 n = Cross(v[1]-v[0], v[2]-v[0]);
+    /* Normalized the normal vector */
+    float d = Length(n);
+    n = n/d;
+    return n;
+  }
+  Face() = default;
+   ~Face() = default;
 };
 
 struct Mesh3 {
-    Point3 v[8]; // vetex: counterclockwise, and from z = 0 plane to z = +z plane
-    Mesh3 (Point3 A, Point3 B, Point3 C, Point3 D, Point3 E, Point3 F, Point3 G, Point3 H) {
-        this->v[0] = A;
-        this->v[1] = B;
-        this->v[2] = C;
-        this->v[3] = D;
-        this->v[4] = E;
-        this->v[5] = F;
-        this->v[6] = G;
-        this->v[7] = H;
-    }
+  Point3 v[8]; // vetex: counterclockwise, and from z = 0 plane to z = +z plane
+  Mesh3 (Point3 A, Point3 B, Point3 C, Point3 D, Point3 E, Point3 F, Point3 G, Point3 H) {
+    this->v[0] = A;
+    this->v[1] = B;
+    this->v[2] = C;
+    this->v[3] = D;
+    this->v[4] = E;
+    this->v[5] = F;
+    this->v[6] = G;
+    this->v[7] = H;
+  }
 };
 
 bool isPointInPolyhedron(const Point3 &p, const std::vector<Face> &fs);
+
+
 
 #ifdef __cplusplus
 extern "C" {
